97d918996659c72b6d794a5f3c7d01dd89bf4527
peshaladushyanthika <pdushyanthika@gmail.com>
2020-12-30 23:07:34 +0530

    Updated

--- src/main/java/com/temperature/alert/api/Application.java
+++ src/main/java/com/temperature/alert/api/Application.java
@@ -1,14 +1,26 @@
 package com.temperature.alert.api;
 
+import com.temperature.alert.api.notification.EmailSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		public static void main(String[] args) throws MessagingException {
+
+			//Email notification call in here.
+
+
+			String bodyData;
+			EmailSender.sentMail(bodyData: "Temperature sensor -T002 has exceed the 35C threshold and its recode is 38C");
+
+
+			SpringApplication.run(Application.class, args);
 	}
 
 }
